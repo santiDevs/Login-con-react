f59d7cd08224eb353c44773a10f5cec5dc31d364
SantiDevs <santi.dev.art@gmail.com>
2024-04-17 19:29:34 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <section className='login'>
+        <h1>Register</h1>
+        <form action="" className='form-container'>
+          <label htmlFor="Title">Register</label>
+          <br />
+          <input type="text" className='input'/>
+          <br />
+          <input type="text" className='input'/>
+          <br />
+          <button><a href="">register</a></button>
+        </form>
+      </section>
+    </div>
+  )
+}
+
+export default App
